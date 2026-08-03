e28e4af09357d1054f299c084dc8b38957e88675
UMMIDISETTY JAHNAVI <2300040357ece@gmail.com>
2026-08-03 10:54:34 +0530

    Day 2 - Data Types

---
+++ Day02_DataTypes/data_types.py
@@ -0,0 +1,16 @@
+name = "Jahnavi"
+age = 20
+cgpa = 8.24
+is_student = True
+
+print("Name:", name)
+print(type(name))
+
+print("Age:", age)
+print(type(age))
+
+print("CGPA:", cgpa)
+print(type(cgpa))
+
+print("Is Student:", is_student)
+print(type(is_student))
